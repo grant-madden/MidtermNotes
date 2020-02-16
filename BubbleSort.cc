9c3a733003f6e8ae9e7c416db1b5de58cb19f73e
Gerald McGee <44445299+GmcgeeCodes@users.noreply.github.com>
2020-02-16 17:41:08 -0500

    Update BubbleSort.cc

---
+++ BubbleSort.cc
@@ -0,0 +1,11 @@
+void bubblesort(int arr[], int length){
+  // Bubble largest toward the right
+  for (int i = legnth - 1; j < 1; j++)
+    if (arr[j] > arr[j+1])
+      {
+        // swap numbers
+        int temp = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = temp;
+       }
+  }
